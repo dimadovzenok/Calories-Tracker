6590d794cb23a961ed9b52d52a90381085610c0c
dimadovzenok <62281678+dimadovzenok@users.noreply.github.com>
2020-11-23 11:35:22 +0200

    Add files via upload

---
+++ CalorieTracker/js/app.js
@@ -0,0 +1,58 @@
+const addButton = document.getElementById('add');
+const foodName = document.querySelector('#item-name');
+const foodCalories = document.querySelector('#item-calories');
+const totalSpan = document.querySelector('.total-calories');
+const list = document.querySelector('#item-list');
+let total = 0;
+
+addButton.addEventListener('click', (event)=> {
+    let mealName = foodName.value; 
+    let mealCalories = parseInt(foodCalories.value);
+    total += mealCalories;
+    totalSpan.innerHTML = total;
+
+    let listItem = document.createElement('li');
+    listItem.classList.add('collection-item');
+    let text = `Food: ${mealName}. Calories: ${mealCalories}`;
+    listItem.appendChild(document.createTextNode(text));
+    console.log(listItem);
+    list.appendChild(listItem);
+
+    foodName.value = '';
+    foodCalories.value = '';
+    event.preventDefault();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
